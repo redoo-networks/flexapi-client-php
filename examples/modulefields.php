3d7621c566becc9d03aab5111faa1588c7a42cb7
Stefan Warnat <warnat@redoo-networks.com>
2020-01-03 12:37:34 +0100

    Fix Typo

--- examples/modulefields.php
+++ examples/modulefields.php
@@ -5,8 +5,8 @@ require_once('login.php');
 ini_set('display_errors', 1);
 error_reporting(-1);
 
-$modulObj = new \FlexAPI\Model\Module('Accounts');
-$fields = $modulObj->getFields();
+$moduleObj = new \FlexAPI\Model\Module('Accounts');
+$fields = $moduleObj->getFields();
 
 echo '<pre>';
 var_dump($fields);
